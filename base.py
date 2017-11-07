137b70fcc8684293241923c12cb999d0cddcdf0d
Phyzzqs <zhuzq.cn+github@gmail.com>
2017-11-07 16:31:41 +0800

    add echo function

--- base.py
+++ base.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python
 # coding:utf-8
+# base.py
 
 import sys
 import json
 import logging
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+
+Config = {}
 
 def load_conf(conf = "config.json"):
 	f = open(conf, "r")
 	text = f.read()
 	config = json.loads(text)
 	f.close()
-	if (not "token" in config.keys()):
-		print("Config file error")
-		sys.exit(1)
+	nece_keys = ["token", "message"]
+	for key in nece_keys:
+		if not key in config.keys():
+			print("Config file error")
+			sys.exit(1)
 	return config
 	
 def start(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="It's workinng!")
+	bot.send_message(chat_id=update.message.chat_id, text=Config["message"])
+	
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 	
 def main():
-	config = load_conf()
-	logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-	updater = Updater(token=config["token"])
+	Config = load_conf()
+	logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+	updater = Updater(token=Config["token"])
 	dispatcher = updater.dispatcher
 	start_handler = CommandHandler('start', start)
 	dispatcher.add_handler(start_handler)
+	echo_handler = MessageHandler(Filters.text, echo)
+	dispatcher.add_handler(echo_handler)
 	updater.start_polling()
 	
 if __name__ == "__main__":

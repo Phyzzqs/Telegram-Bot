fef020b58510b2888e864602e27ae597d7bf9138
Phyzzqs <zhuzq.cn+github@gmail.com>
2017-11-08 17:17:35 +0800

    add functions

--- app/plugins/tuling.py
+++ app/plugins/tuling.py
@@ -5,13 +5,15 @@
 import requests
 import logging
 import sys
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import MessageHandler, Filters, CommandHandler
 from ..main import Bot
 
 @Bot.plugin_register('tuling')
 class Tuling:
     key = ""
     api = "http://www.tuling123.com/openapi/api"
+    botname = ""
+    listen_group = False
     
     def deal(self, re):
         if re["code"] == 100000:
@@ -27,10 +29,10 @@ class Tuling:
             return 1
         elif re["code"] == 40002:
             logging.warn("Empty info.")
-            return 1
+            return "你在说什么，我听不清"
         elif re["code"] == 40004:
             logging.warn("The number of requests has been exhausted.")
-            return "我累了，明天再聊。"
+            return "我累了，明天再聊"
         elif re["code"] == 40007:
             logging.warn("Data format is illegal.")
             return 1
@@ -44,17 +46,45 @@ class Tuling:
         re = requests.post(self.api, data = data).json()
         return self.deal(re)
 
-        
-    def echo(self, bot, update):
-        text = self.get_message(update.message.text, update.message.from_user.username)
+    def process(self, bot, message, chat_id, uid):
+        text = self.get_message(message, uid)
         if sys.version > "3":
             if isinstance(text, str):
-                bot.send_message(chat_id=update.message.chat_id, text=text)
+                bot.send_message(chat_id=chat_id, text=text)
         else:
-            if isinstance(text, (str, unicode)):
-                bot.send_message(chat_id=update.message.chat_id, text=text)
+            if isinstance(text, unicode):
+                bot.send_message(chat_id=chat_id, text=text)
+    
+    def chat(self, bot, update):
+        self.process(bot, update.message.text.lstrip("/chat"), 
+            update.message.chat_id, 
+            update.message.from_user.id)
+            
+    def echo(self, bot, update):
+        if update.message.chat.type != "group":
+            self.process(bot, update.message.text, 
+                update.message.chat_id, 
+                update.message.from_user.id)
+        elif self.listen_group:
+            if update.message.reply_to_message != None:
+                if update.message.reply_to_message.from_user.username == self.botname:
+                    self.process(bot, update.message.text, 
+                        update.message.chat_id, 
+                        update.message.from_user.id)
+            elif update.message.text.find("@"+self.botname) == 0:
+                self.process(bot, update.message.text.lstrip("@"+self.botname), 
+                    update.message.chat_id, 
+                    update.message.from_user.id)
         
-    def process(self, updater, dispatcher, config):
+    def init(self, updater, dispatcher, config):
         self.key = config["key"]
+        if config["listen_group"]:
+            if config.get("botname", "") == "":
+                logging.warn("Bot name is needed to listen group")
+            else:
+                self.listen_group = True
+                self.botname = config["botname"]
         echo_handler = MessageHandler(Filters.text, self.echo)
         dispatcher.add_handler(echo_handler)
+        chat_handler = CommandHandler("chat", self.chat)
+        dispatcher.add_handler(chat_handler)
